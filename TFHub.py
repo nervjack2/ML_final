854b16f39e08a551932a2f28b5e33248c12206b3
unknown <b07902042@ntu.edu.tw>
2020-12-30 19:41:50 +0800

    a

---
+++ TFHub.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import re
+from clean import *
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow_hub as hub
+
+import tokenization
+
+
+def bert_encode(texts, tokenizer, max_len=256):
+    all_tokens = []
+    all_masks = []
+    all_segments = []
+    ii=0
+    f=open("./data1.csv", 'w') 
+    for text in texts:
+        ii+=1
+        text = cleaning(text)
+        text = tokenizer.tokenize(text)
+        f.write(f'{ii},{text}\n')   
+        text = text[:max_len-2]
+        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        pad_len = max_len - len(input_sequence)
+        
+        tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+        tokens += [0] * pad_len
+        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        segment_ids = [0] * max_len
+        
+        all_tokens.append(tokens)
+        all_masks.append(pad_masks)
+        all_segments.append(segment_ids)
+    
+    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+
+def build_model(bert_layer, max_len=256):
+    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+    input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+    segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+
+    _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+    clf_output = sequence_output[:, 0, :]
+    out = Dense(1, activation='sigmoid')(clf_output)
+    
+    model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+    model.compile(Adam(lr=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+    
+    return model
+config=tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.compat.v1.Session(config = config)
+mp ="/tmp2/b07902042/model.h5"
+module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
+bert_layer = hub.KerasLayer(module_url, trainable=True)
+
+train = pd.read_csv("./data/train.csv")
+test = pd.read_csv("./data/test.csv")
+submission = pd.read_csv("./data/sample_submission.csv")
+
+vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
+
+train_input = bert_encode(train.text.values, tokenizer, max_len=80)
+test_input = bert_encode(test.text.values, tokenizer, max_len=80)
+train_labels = train.target.values
+#mp = './model.h5'
+model = build_model(bert_layer, max_len=80)
+model.summary()
+
+checkpoint = ModelCheckpoint(mp, monitor='val_loss', save_best_only=True)
+
+train_history = model.fit(
+    train_input, train_labels,
+    validation_split=0.2,
+    epochs=3,
+    callbacks=[checkpoint],
+    batch_size=10
+)
+model.load_weights(mp)
+test_pred = model.predict(test_input)
+
+submission['target'] = test_pred.round().astype(int)
+submission.to_csv('submission.csv', index=False)
