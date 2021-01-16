4f5ac9fa6633bc557e848960eafa4ad2f44236f5
Darwin <b07902052@ntu.edu.tw>
2021-01-16 18:23:48 +0800

    modifiable path

--- TFHub.py
+++ TFHub.py
@@ -8,8 +8,16 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow_hub as hub
+import sys
+import os
 
 import tokenization
+
+train_fpath = sys.argv[1]
+test_fpath = sys.argv[2]
+sample_submission_fpath = sys.argv[3]
+submission_fpath = sys.argv[4]
+model_dir = sys.argv[5]
 
 
 def bert_encode(texts, tokenizer, max_len=256):
@@ -17,25 +25,23 @@ def bert_encode(texts, tokenizer, max_len=256):
     all_masks = []
     all_segments = []
     ii=0
-    f=open("./data1.csv", 'w') 
     for text in texts:
         ii+=1
         text = cleaning(text)
         text = tokenizer.tokenize(text)
-        f.write(f'{ii},{text}\n')   
         text = text[:max_len-2]
         input_sequence = ["[CLS]"] + text + ["[SEP]"]
         pad_len = max_len - len(input_sequence)
-        
+
         tokens = tokenizer.convert_tokens_to_ids(input_sequence)
         tokens += [0] * pad_len
         pad_masks = [1] * len(input_sequence) + [0] * pad_len
         segment_ids = [0] * max_len
-        
+
         all_tokens.append(tokens)
         all_masks.append(pad_masks)
         all_segments.append(segment_ids)
-    
+
     return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
 
 
@@ -47,10 +53,10 @@ def build_model(bert_layer, max_len=256):
     _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
     clf_output = sequence_output[:, 0, :]
     out = Dense(1, activation='sigmoid')(clf_output)
-    
+
     model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
     model.compile(Adam(lr=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
-    
+
     return model
 config=tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -59,9 +65,9 @@ sess = tf.compat.v1.Session(config = config)
 module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
 bert_layer = hub.KerasLayer(module_url, trainable=True)
 
-train = pd.read_csv("./data/train.csv")
-test = pd.read_csv("./data/test.csv")
-submission = pd.read_csv("./data/sample_submission.csv")
+train = pd.read_csv(train_fpath)
+test = pd.read_csv(test_fpath)
+submission = pd.read_csv(sample_submission_fpath)
 
 vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
 do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
@@ -70,7 +76,7 @@ tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
 train_input = bert_encode(train.text.values, tokenizer, max_len=80)
 test_input = bert_encode(test.text.values, tokenizer, max_len=80)
 train_labels = train.target.values
-mp = './model.h5'
+mp = os.path.join(model_dir, 'model.h5')
 model = build_model(bert_layer, max_len=80)
 model.summary()
 
@@ -87,4 +93,4 @@ model.load_weights(mp)
 test_pred = model.predict(test_input)
 
 submission['target'] = test_pred.round().astype(int)
-submission.to_csv('submission.csv', index=False)
+submission.to_csv(submission_fpath, index=False)
